bc6e3c09c3775d105a5bc2f25332fb7ce7501910
Eric Churchill <eric.churchill2@gmail.com>
2017-02-16 17:14:42 -0800

    test(Tests): write tests for getFirstWordFromDictionary functionality

--- src/project/tests/Tests.elm
+++ src/project/tests/Tests.elm
@@ -1,5 +1,6 @@
 module Tests exposing (..)
 
+import Array exposing (Array)
 import Test exposing (..)
 import Expect exposing (..)
 import ElmTestBDDStyle exposing (..)
@@ -9,8 +10,24 @@ import Hangman
 suite : Test
 suite =
     describe "Validation Suite"
-        [ describe "update functionality"
-            [ it "can handle Reset messages" <|
+        [ describe "getFirstWordFromDictionary"
+            [ it "returns first word from list when word exists" <|
+                let
+                    dictionary =
+                        Array.fromList [ "approvingly", "carnivals" ]
+                in
+                    expect (Hangman.getFirstWordFromDictionary dictionary) to equal <|
+                        "approvingly"
+            , it "returns a String when array is empty" <|
+                let
+                    dictionary =
+                        Array.fromList []
+                in
+                    expect (Hangman.getFirstWordFromDictionary dictionary) to equal <|
+                        "Error: Word not found!"
+            ]
+        , describe "update functionality"
+            [ it "handles Reset messages" <|
                 expect (Hangman.update Hangman.Reset Hangman.model) to equal <|
                     Hangman.model
             ]
